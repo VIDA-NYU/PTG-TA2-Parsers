5ef172e569b52515114f1c6b3001ab668fc1877f
Joao Rulff <jlrulff@gmail.com>
2023-05-22 07:39:25 -0400

    adding bash script to run all parsers

--- ngc-ocarina/run.sh
+++ ngc-ocarina/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-inputDB='/faststorage/OCARINA_ExtractedData/0293/0293_11.sqlite'
+inputDB='/faststorage/OCARINA_ExtractedData/0293/0293_13.sqlite'
 outputPath='/home/joaorulff/Workspace/PTG/PTG-TA2-Parsers/data/output'
+videoPath='/home/joaorulff/Workspace/PTG/PTG-TA2-Parsers/data/output/main.mp4'
+
+## ADD path to the 3D model
+# model='/foo/bar/model.fbx'
 
 echo "Generating eye data"
 python eye-parser.py -i $inputDB -o $outputPath
@@ -10,4 +14,11 @@ python perception-parser.py -i $inputDB -o $outputPath
 
 echo "Generating reasoning data"
 python reasoning-parser.py -i $inputDB -o $outputPath
+
+# echo "Generating video"
+python image_extractor.py
+
+echo "Generating medatada"
+python generate-metadata.py -i $videoPath -o $outputPath
+
 
